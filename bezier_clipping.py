[fix] Calculation error bug fixed
- Calculation error bug fixed
- Rename main.py
- Bezier Clipping bug fixed
- Update README.md

bezier_clipping.py
@@ -133,10 +133,13 @@ class BezierClipping:
 		t_min = t[0].x
 		t_max = t[-1].x
 
+		next_line = PlaneLine([(t_min, 0), (t_max, 0)])
+		if next_line.length <= dic["precision"]:
+			return next_line.midpoint.x
+
 		bez, _ = dic["base_bezier"].split(t_max)
 		bez_t = t_min / t_max
 		_, next_bezier = bez.split(bez_t)
-		next_line = PlaneLine([(t_min, 0), (t_max, 0)])			
 
 		if np.abs(dic["current_line"].length - next_line.length) < 1e-6:
 			## Target has more than 2 intersection
@@ -149,8 +152,6 @@ class BezierClipping:
 			dic2["current_line"] = next_line
 			return self._clipping(dic1), self._clipping(dic2)
 
-		if next_line.length	<= dic["precision"]:
-			return next_line.midpoint.x
 		dic["current_bezier"] = next_bezier
 		dic["current_line"] = next_line
 		return self._clipping(dic)

line_module.py
@@ -173,8 +173,8 @@ class Bezier:
 		-------
 		t must be in the range 0 to 1.
 		"""
-		if t < 0 or 1 < t:
-			msg = "t must be in the range 0 to 1"
+		if t < -1e-8 or 1 + 1e-8 < t:
+			msg = "t must be in the range 0 to 1, but get " + str(t)
 			raise ValueError(msg)
 
 		p = np.zeros(2)
@@ -229,8 +229,8 @@ class Bezier:
 		-------
 		t must be in the range 0 to 1.
 		"""
-		if t < 0 or 1 < t:
-			msg = "t must be in the range 0 to 1"
+		if t < -1e-8 or 1 + 1e-8 < t:
+			msg = "t must be in the range 0 to 1, but get " + str(t)
 			raise ValueError(msg)
 
 		M = self.create_Ux()